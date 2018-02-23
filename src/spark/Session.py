1c35e7b08f046fb9a03719c87f8510167b2888a4
weiliang.liu <fnst#1234>
2018-02-23 14:00:09 +0800

    init

---
+++ src/spark/Session.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@version: 1.0.0
+@author: bright
+@contact: bright.liu2015@foxmail.com
+@file: Session.py
+@time: 23/02/2018 13:53
+"""
+
+if __name__ == '__main__':
+    pass
---
+++ src/spark/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@version: 1.0.0
+@author: bright
+@contact: weiliang.liu@sky-data.cn
+@file: __init__.py.py
+@time: 23/02/2018 13:52
+"""
+
+if __name__ == '__main__':
+    pass
